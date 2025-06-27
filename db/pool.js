cbd741a2e3b5388c6a23148c607808841d72e8bd
brianooi <briansh1225@gmail.com>
2025-06-27 19:17:10 +0800

    fix db connection

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD, // optional if not using password auth
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_DB_NAME,
+  password: process.env.DATABASE_DB_PASSWORD, // optional if not using password auth
   port: process.env.DB_PORT || 5432,
   ssl: { rejectUnauthorized: false },
 });
