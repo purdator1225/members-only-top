c5861970ad4c21022bc74646fd42a63476b67be6
brianooi <briansh1225@gmail.com>
2025-06-27 18:26:54 +0800

    removed node-watch and add rejectunauthorised false

--- db/pool.js
+++ db/pool.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD, // optional if not using password auth
   port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
